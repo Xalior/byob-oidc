64b974ee945eaf192a1573e1ab2f352d809e7e31
D. Rimron-Soutter <darran@xalior.com>
2026-01-10 13:45:50 +0000

    Bring the (vapor-wave) funk back...

--- src/themes/xalior/theme.ts
+++ src/themes/xalior/theme.ts
@@ -1,7 +1,6 @@
-export default {
-    name: 'xalior',
-    page: (html: string)  => {
-        return `<!DOCTYPE html>
+const single_render = (html: string)  => {
+    console.log("SINGLE RENDERER");
+    return `<!DOCTYPE html>
         <html>
             <head>
                 <meta charset="utf-8">
@@ -27,8 +26,6 @@ export default {
                         <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"></path>
                     </symbol>
                 </svg>
-
-                    <a class="nav-link active" aria-current="page" href="/"><img src="/theme/nbn-id.png" width="57"></a>
 
                 <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
                     <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (dark)">
@@ -60,7 +57,7 @@ export default {
                     </ul>
                 </div>
                 ` +
-            `<section class="w-100">
+        `<section class="w-100">
                     <div class="container">
                         ${html}
                     </div>
@@ -71,5 +68,20 @@ export default {
             <script src="/theme/main.js"></script>            
             </body>
         </html>`;
-    }
+}
+
+export default {
+    name: 'xalior',
+    page: (html: string)  => single_render(html),
+    logout: (form: string, hostname: string)  => {
+        return single_render(`<h1>Do you want to sign-out from the Single Sign-On (SSO) System at ${hostname} too?</h1>
+                    ${form}
+                    <button autofocus type="submit" form="op.logoutForm" value="yes" name="logout">Yes, sign me out</button>
+                    <button type="submit" form="op.logoutForm">No, stay signed in</button>`);
+    },
+    loggedout: (html: string)  => {
+        return single_render(`<h1>Sign-out Success</h1>
+                                <p>Your sign-out ${html ? `with ${html}` : ''} was successful.</p>`);
+    },
+    error: (html: string)  => single_render(html)
 };
